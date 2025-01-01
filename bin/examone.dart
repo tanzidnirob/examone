a88d4fd3e5a49700f2d7a563c662f49d71f886a8
Tanzid <tanzidnirob05@gmail.com>
2025-01-01 23:10:08 +0600

    final push

---
+++ bin/examone.dart
@@ -0,0 +1,12 @@
+void main() {
+  triangle();
+}
+
+void triangle() {
+  double base = 10.0;
+  double height = 5.0;
+
+  double area = 0.5 * base * height;
+
+  print("The area of the triangle is: $area");
+}
